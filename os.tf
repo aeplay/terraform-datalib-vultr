29ce05b6cad7a3b70fe6781ad09e2d820506da34
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2020-05-27 09:31:32 +0200

    monkey patch: remove old fedora causing error

--- os.tf
+++ os.tf
@@ -76,10 +76,6 @@ output "freebsd_12_x64" {
   value = local.os_zipped["FreeBSD 12 x64"]
 }
 
-output "fedora_30_x64" {
-  value = local.os_zipped["Fedora 30 x64"]
-}
-
 output "debian_10_x64" {
   value = local.os_zipped["Debian 10 x64 (buster)"]
 }
